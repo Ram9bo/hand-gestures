a883214e223c13c33a55bf020a7e49468c4f3323
Mireia <mireiaplalis@github.com>
2023-05-16 11:34:35 +0200

    two hand

--- direction.py
+++ direction.py
@@ -1,22 +1,56 @@
 import numpy as np
 
-def detect_direction(landmarks):
-    directions = ["right", "up", "left", "down"]
-    tips = [4, 8, 12, 16, 20]
-    wrist_pos = landmarks[0]
-    big_dist = 0
-    big_vect = None
-    for tip in tips:
-        tip_pos = landmarks[tip]
-        vector = tip_pos - wrist_pos
-        dist = np.linalg.norm(vector)
-        if dist > big_dist:
-            big_dist = dist
-            big_vect = vector
-    coord = np.argmin(big_vect)
-    coord = coord + 2 if big_vect[coord] < 0 else coord
-    direction = directions[coord]
-    return direction
-    
+def detect_direction(all_landmarks):
+    detected_directions = []
+    for hand_landmarks in all_landmarks:
+        extended_counter = 0
+        directions = ["left", "down", "right", "up"]
+        tips = [4, 8, 12, 16, 20]
+        midfinger = [2, 6, 10, 14, 18]
+        lower_finger = [2, 5, 9, 13, 17]
+
+        # for tip in tips:
+        #     print("tip number: ", tip)
+        #     print("coordinates: ", hand_landmarks[tip])
+        #     vect = hand_landmarks[tip]-hand_landmarks[0]
+        #     print("vector: ", vect)
+        #     coord = np.argmax(np.abs(vect))
+        #     coord = coord + 2 if vect[coord] < 0 else coord
+        #     direction = directions[coord]
+        #     print("direction: ", direction)
+        # import pdb; pdb.set_trace()
 
 
+        wrist_pos = hand_landmarks[0]
+        big_dist = 0
+        big_vect = None
+        for i, tip in enumerate(tips):
+            tip_pos = hand_landmarks[tip]
+            midfinger_pos = hand_landmarks[midfinger[i]]
+            # Thumb
+            if tip == 4:
+                # If thumb close to any midfinger, not extended thumb
+                thumb_length = np.linalg.norm(tip_pos-midfinger_pos)
+                extended_thumb = True
+                for j in range(i+1, len(tips)):
+                    dist1 = np.linalg.norm(tip_pos-hand_landmarks[midfinger[j]])
+                    dist2 = np.linalg.norm(tip_pos-hand_landmarks[lower_finger[j]])
+                    if dist1 < thumb_length or dist2 < thumb_length:
+                        extended_thumb = False
+                        break
+                extended_counter += int(extended_thumb)
+            else:
+                upper_vect = tip_pos-midfinger_pos
+                lower_vect = midfinger_pos - wrist_pos
+                extended_counter += int(np.dot(upper_vect, lower_vect) > 0)
+
+            vector = tip_pos - wrist_pos
+            dist = np.linalg.norm(vector)
+            if dist > big_dist:
+                big_dist = dist
+                big_vect = vector
+        coord = np.argmax(np.abs(big_vect))
+        coord = coord + 2 if big_vect[coord] < 0 else coord
+        direction = directions[coord]
+        detected_directions.append([direction, extended_counter])
+    return detected_directions
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import cv2
 import mediapipe as mp
+from direction import detect_direction
 
-
-def detect_hands(image):
+def detect_hands(image, hands):
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # Initialize the hand landmark model
@@ -11,10 +11,18 @@ def detect_hands(image):
     # Process the image
     results = mp_hands.process(image_rgb)
 
+    if len(hands) == 1:
+        for i, h in enumerate(results.handedness):
+            if hands[0] in h[0].display_name:
+                filtered_hand_landmarks = results.multi_hand_landmarks[i]
+                break
+    else:
+        filtered_hand_landmarks = results.multi_hand_landmarks
+
     marks = []
     # Check if landmarks were detected
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
+    if filtered_hand_landmarks:
+        for hand_landmarks in filtered_hand_landmarks:
             landmarks = []
             # Draw the landmarks on the image
             mp.solutions.drawing_utils.draw_landmarks(image, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
@@ -32,9 +40,43 @@ if __name__ == '__main__':
     cap.set(3, 1280)
     cap.set(4, 720)
 
+    prev_gesture = None
+    prev_direction = None
+    print("Type how many hands you want to use for gesture control: (1 / 2)")
+    num_hands = input()
+    if num_hands == "1":
+        print("Type which hand you want to use for gesture control: (L / R)")
+        hand = input().lower()
+        if (hand == "l" or hand == "left"):
+            hands = ["L"]
+        elif (hand == "r" or hand == "right"):
+            hands = ["R"]
+        else:
+            raise TypeError("The input received doesn't correspond to a hand")
+    else:
+        hands = ["L", "R"]
+
     while True:
         ret, frame = cap.read()
-        frame, marks = detect_hands(frame)
+        frame, all_landmarks = detect_hands(frame)
         # Display the image with landmarks
         cv2.imshow('Hand Landmarks', frame)
         cv2.waitKey(1)
+
+        speed = 0
+        angle = 0
+
+        if len(all_landmarks) > 0:
+            direction = detect_direction(all_landmarks)
+            print("Direction", direction)
+            for hand_command in direction:
+                direction = hand_command[0]
+                fingers = hand_command[1]
+                if direction == "up":
+                    speed += fingers
+                elif direction == "down":
+                    speed -= fingers
+                elif direction == "right":
+                    angle += fingers
+                elif direction == "left":
+                    angle -= fingers
